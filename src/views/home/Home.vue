1cf2ff7e9015ae0c58762c2da11452d18c39b164
ouranjun <51231300+ouranjun@users.noreply.github.com>
2020-01-27 14:31:03 +0800

    添加navbar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="home">
-    首页
+    <!-- 导航栏 -->
+    <nav-bar class="nav-bar">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from 'common/navbar/NavBar'
 
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    NavBar
+  }
 }
 </script>
+
+<style scoped>
+.nav-bar {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
+</style>
