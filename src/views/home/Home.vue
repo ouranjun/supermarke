4c406748cec9b30575cefae1f560382eb9f37fb4
ouranjun <51231300+ouranjun@users.noreply.github.com>
2020-01-27 15:34:13 +0800

    备份

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,16 +4,34 @@
     <nav-bar class="nav-bar">
       <div slot="center">购物街</div>
     </nav-bar>
+    <homeSwiper :banners='banners' />
   </div>
 </template>
 
 <script>
 import NavBar from 'common/navbar/NavBar'
+import homeSwiper from './chilrenCom/homeSwiper'
+import {getHomeMultidata} from 'network/home'
 
 export default {
   name: 'home',
+  data () {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
   components: {
-    NavBar
+    NavBar,
+    homeSwiper
+  },
+  created () {
+    // 1. 请求多个数据
+    getHomeMultidata().then(res => {
+      // console.log(res);
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
   }
 }
 </script>
---
+++ src/views/home/chilrenCom/homeSwiper.vue
@@ -0,0 +1,39 @@
+<template>
+  <swipe :auto="4000">
+  <swipe-item>122222</swipe-item>
+  <swipe-item>233333</swipe-item>
+  <swipe-item>333333</swipe-item>
+  </swipe>
+</template>
+
+<script>
+import { Swipe, SwipeItem } from 'mint-ui';
+
+import 'mint-ui/lib/style.css'
+
+export default {
+  name: 'homeSwiper',
+  props: {
+    banners: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    recommends: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  components: {
+    Swipe, 
+    SwipeItem
+  }
+}
+</script>
+
+<style>
+
+</style>
